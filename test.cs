9fc86da751f0123e0aaf91a5026d0b23bc975813
Johannes Sluis <joesluis51@gmail.com>
2024-11-07 08:05:33 -0800

    testing C#

---
+++ test.cs
@@ -0,0 +1,6 @@
+// Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+public class Solution {
+    public bool hasDuplicate(int[] nums) {
+
+    }
+}
