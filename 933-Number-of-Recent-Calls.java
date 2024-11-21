28ba17fa9023657bbd97ea34f9cfd0cb0f4a0b78
Johannes Sluis <43276756+JoeS51@users.noreply.github.com>
2024-11-20 22:24:25 -0800

    [933] [Time Beats: 12.18%] [Memory Beats: 17.09%] - LeetPush

---
+++ 933-Number-of-Recent-Calls.java
@@ -0,0 +1,26 @@
+class RecentCounter {
+    List<Integer> l;
+    public RecentCounter() {
+        l = new ArrayList<>();
+    }
+    
+    public int ping(int t) {
+        int count = 1;
+        
+        for (int i = l.size() - 1; i >= 0; i--) {
+            if (l.get(i) < t - 3000) {
+                l.add(t);
+                return count;
+            }
+            count++;
+        }
+        l.add(t);
+        return count;
+    }
+}
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter obj = new RecentCounter();
+ * int param_1 = obj.ping(t);
+ */
